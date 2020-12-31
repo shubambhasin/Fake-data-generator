9bdda2cef54e1edab87fbf2b3df61d181350eaae
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-31 23:12:55 +0530

    page refresh option added

--- src/Components/button/Button.js
+++ src/Components/button/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "../button/Button.css";
 const Button = ({ btnName = "Button Name" }) => {
-  return <button className="btn-primary">{btnName}</button>;
+  return (
+    <button
+      onClick={() => window.location.reload(false)}
+      className="btn-primary"
+    >
+      {btnName}
+    </button>
+  );
 };
 
 export default Button;
